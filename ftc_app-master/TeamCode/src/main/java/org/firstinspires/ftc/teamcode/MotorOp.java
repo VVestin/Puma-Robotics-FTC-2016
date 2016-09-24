77757b840538e36885c1d45e398738fbe6832f8e
Vincent-MaP2P <vwma01@bvsd.org>
2016-09-24 13:51:36 -0600

    testing vcs

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Me on 9/24/16.
+ */
+public class MotorOp extends OpMode {
+    private DcMotor motor;
+
+    public void init(){
+        motor=hardwareMap.dcMotor.get("Motor A");
+    }
+
+    public void loop(){
+        motor.setPower(1);
+    }
+}
