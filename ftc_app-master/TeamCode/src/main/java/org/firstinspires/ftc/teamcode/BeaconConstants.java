14c8a7d7135ea0109ee1c55c633642b2f4f53ef5
Vincent-MaP2P <vwma01@bvsd.org>
2017-01-16 14:25:53 -0700

    Final changes before qualifier

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconConstants.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconConstants.java
@@ -22,10 +22,10 @@ public interface BeaconConstants {
     public static final double FRONT_WHITE_THRESHOLD = 10;
     public static final double CR_POWER = 0.3;
     public static final double SECOND_BEACON_DISTANCE = 30;
-    public static final double BEACON_FOUND_THRESHOLD = 5;
+    public static final double BEACON_FOUND_THRESHOLD = 4;
     public static final long CR_CENTER_TIME = 1126;
     public static final int SECOND_TURN_ANGLE = 80;
-    public static final double FIND_BEACON_POWER = 0.1; // 0.1
+    public static final double FIND_BEACON_POWER = 0.2; // 0.1
     public static final double PUSH_BUTTON_POWER = 0.15; // 0.1
     public static final double CS_BLACK_THRESHOLD = 3;
     public static final double REALIGN_TIME_THRESHOLD = 0.6; // 0.6
